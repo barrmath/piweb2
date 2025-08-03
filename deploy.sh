5938c0da3206bcc531416b8e6714c57619675048
barrmath <mathieu.barrios@outlook.com>
2025-08-03 19:20:11 +0200

    erreur 2 http// dans le deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 git push origin main
-curl --user $USER:$TOKENJENKINS http://http://tuxpi:8080/job/piweb_deploy/buildWithParameters?token=$TOKENJENKINS
+curl --user $USER:$TOKENJENKINS http://tuxpi:8080/job/piweb_deploy/buildWithParameters?token=$TOKENJENKINS
