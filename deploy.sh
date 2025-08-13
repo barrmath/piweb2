5dc937127aec314e29bd7999bde840d55683ec8a
barrmath <mathieu.barrios@outlook.com>
2025-08-13 15:27:45 +0200

    mise en variable d'environnement adresse jenkins

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 git push origin main
-curl --user $USER:$JENKINSUSERTOKEN http://tuxpi:8080/job/piweb/buildWithParameters?token=$TOKENJENKINS
+curl --user $USER:$JENKINSUSERTOKEN $URLJENKINS$TOKENJENKINS
